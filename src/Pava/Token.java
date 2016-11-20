4a53b06c659129a3802ceaaab7788a4b26afb634
C.y <yangruihan@vip.qq.com>
2016-11-20 15:08:17 +0800

    Pava v0.0.3 1. Add abstract class Token

---
+++ src/Pava/Token.java
@@ -0,0 +1,43 @@
+package Pava;
+
+/**
+ * Token class
+ * Created by yangruihan on 2016/11/20.
+ */
+public abstract class Token {
+    public static final Token EOF = new Token(-1) {
+    };       // end of file
+    public static final String EOL = "\\n";                 // end of line
+
+    private int lineNumber;     // the line number of this token
+
+    protected Token(int line) {
+        lineNumber = line;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    /* type of token */
+    public boolean isIdentifier() {
+        return false;
+    }
+
+    public boolean isNumber() {
+        return false;
+    }
+
+    public boolean isString() {
+        return false;
+    }
+
+    /* get value from token */
+    public int getNumber() {
+        return -1;
+    }
+
+    public String getString() {
+        return "";
+    }
+}
